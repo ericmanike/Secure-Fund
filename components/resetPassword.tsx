6139e8ad702f5fc2d20f45576e7ca40bc107cdc9
Ericsson <manikeeric@gmail.com>
2026-01-03 08:20:12 +0000

    suspense fixed

---
+++ components/resetPassword.tsx
@@ -0,0 +1,138 @@
+'use client'
+import { useState } from 'react'
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import ResetSuspense from '@/components/resetSuspense'
+
+import { ArrowLeft, Eye, EyeOff} from 'lucide-react'
+ import { useSearchParams,useRouter} from 'next/navigation'
+
+
+
+const newPasswordValidationSchema = Yup.object().shape({
+  newPassword: Yup.string().min(6, 'Too short').required('New password is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('newPassword')], 'Passwords must match')
+    .required('Confirm password is required'),
+})
+
+export default function ResetPasswordForm() {
+
+
+ const  Router = useRouter()
+   const [isSending, setIsSending] = useState(false);
+
+  const [showNewPassword, setShowNewPassword] = useState(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+  const searchParams = useSearchParams()
+  const email = searchParams.get('email') || ''
+  const token = searchParams.get('token') || ''
+  const [message, setMessage] = useState<string | null>(null);
+
+
+
+  const resetPassword = async (newPassword: string) => {
+     setIsSending(true);
+     if (!token || !email) {
+   
+      setIsSending(false);
+      return;
+    }
+    try {
+      const res = await fetch('http://localhost:3000/api/auth/resetPassword', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({token:token,email:email,newPassword:newPassword } ),
+      })
+      if (!res.ok) {
+        console.log(' this is the token'+ token)
+        console.log(' this is the email'+ email)
+        console.log(' this is the new password'+ newPassword)
+        console.log('response', res)
+        
+         throw new Error('Password reset failed')
+      }
+      
+      const data = await res.json()
+      setMessage(data.message || "Password reset successful");
+      console.log('Password reset successful:', data)
+  
+      Router.replace('/login')
+    } catch (err: any) {
+
+      console.error(err)
+    }finally {
+      setIsSending(false);
+    }
+  }
+
+  return (
+    <ResetSuspense>
+    <div className="h-screen flex justify-center items-center  p-10 m-auto  md:w-1/2">
+   
+      
+       <button onClick={() => Router.replace('/Login')} className="absolute top-4 left-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded inline-flex items-center">
+          <ArrowLeft/>
+         <span>Back</span>
+       </button>
+      { (
+        <Formik 
+          initialValues={{ newPassword: '', confirmPassword: '' }}
+          validationSchema={newPasswordValidationSchema}
+          onSubmit={(values) => resetPassword(values.newPassword)}
+        >
+          {({ isSubmitting, isValid, dirty }) => (
+            <Form className="bg-white p-6 rounded shadow-md w-full  md:w-[80%]  h-[fit]    flex flex-col gap-4">
+              <h2 className="text-xl font-bold text-center">Enter New Password</h2>
+              <label>New Password</label>
+              <div className="relative">
+                <Field 
+                  type={showNewPassword ? "text" : "password"} 
+                  name="newPassword" 
+                  className="border p-2 rounded w-full pr-10" 
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowNewPassword(!showNewPassword)}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                >
+                  {showNewPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
+              <ErrorMessage name="newPassword" component="div" className="text-red-500 text-sm" />
+
+              <label>Confirm Password</label>
+              <div className="relative">
+                <Field 
+                  type={showConfirmPassword ? "text" : "password"} 
+                  name="confirmPassword" 
+                  className="border p-2 rounded w-full pr-10" 
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                >
+                  {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
+              <ErrorMessage name="confirmPassword" component="div" className="text-red-500 text-sm" />
+
+              <button
+                type="submit"
+                disabled={!dirty || !isValid || isSubmitting}
+              className={` ${ !dirty || !isValid ? 'bg-gray-600 cursor-not-allowed ' : 'bg-blue-500 cursor-pointer'} text-white p-2 rounded transition-colors`}
+              >
+                {isSending ? 'Reseting...' : 'Reset Password'} 
+              </button>
+
+                {message && <p className="text-center text-red-500">{message}</p>}
+            </Form>
+          )}
+        </Formik>
+      )}
+    </div>
+    </ResetSuspense>
+  )
+}
---
+++ components/resetSuspense.tsx
@@ -0,0 +1,14 @@
+import { Suspense} from "react";
+
+
+export default function ResetSuspense({children}: {children: React.ReactNode}) {
+  return (
+    <Suspense fallback={
+      <div className="flex justify-center items-center h-48">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
+    }>
+      {children}
+    </Suspense>
+  );
+}
