6bcf7b17dd1ed68ca30a591528290127ccbeb9c8
Ericsson <manikeeric@gmail.com>
2026-01-04 21:36:05 +0000

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <section className="  py-20">
         <div className="container mx-auto px-4">
           <div className="max-w-3xl mx-auto text-center">
-            <h1 className="text-3xl md:text-5xl font-bold mb-6">
+            <h1 className="text-2xl md:text-3xl font-bold mb-6">
               Nyamekye Loans- 
                <span className=' p-3 shadow-lg'>
               <TextType text={['Accessible Loans','Made Easy','For Students']}
@@ -47,7 +47,7 @@ export default function Home() {
       <section className="py-16 bg-slate-300">
         <div className="container mx-auto px-4">
           <h2 className="text-2xl md:text-3xl font-bold text-center mb-12 text-gray-800">
-            Benefits for Ghanaian Students
+            Benefits for  Students
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
             <div className="bg-primary-50 p-6 rounded-lg shadow-md hover:shadow-lg transition-shadow text-center">
